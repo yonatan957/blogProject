8e0062c781fe915def3b9e6df43dac0fa32e7603
yonatan957 <yonatan22660@gmail.com>
2024-10-10 11:49:47 +0300

    create user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,32 @@
 import { Request, Response } from "express";
 import User, { IUser } from "../models/userModel";
 
-export const createUser = async (req: Request, res: Response) => {};
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        const newUser = new User(req.body)
+        const user = await newUser.save()
+        res.status(201).send()
+    } catch (error) {
+        res.status(400).json(error)
+    }
+};
 
-export const getUsers = async (req: Request, res: Response) => {};
+export const getUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await User.find()
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+};
 
-export const getUser = async (req: Request, res: Response) => {};
+export const getUser = async (req: Request, res: Response) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+};
 
 // Optionally, add DELETE and EDIT functions
